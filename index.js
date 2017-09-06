83a4fc5339455a7241a143f678f083d72c289524
Jevon Wallace <jw0155039@otc.edu>
2017-09-06 13:20:23 -0500

    HBD!

---
+++ index.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.bgGreen.magenta('Happy Birthday!') + (' ') + (chalk.green('To') + (' ') + (chalk.red('You') + (chalk.magenta('!')))));
+console.log(chalk.bgYellow.blue('Happy Birthday!') + (' ') + (chalk.magenta('To') + (' ') + (chalk.blue('You') + (chalk.yellow('!')))));
+console.log(chalk.bgBlue.black('Happy Birthday!') + (' ') + (chalk.green('Dear')));
+console.log(chalk.green('A') + (chalk.yellow('D') + (chalk.gray('A') + (chalk.cyan('M') + (' ') + (chalk.green('E') + (chalk.yellow('N') + (chalk.gray('G') + (chalk.cyan('E')+ (chalk.yellow('B') + (chalk.gray('R') + (chalk.cyan('E')+ (chalk.yellow('T') + (chalk.gray('S') + (chalk.cyan('O') + (chalk.green('N'))))))))))))))));
+console.log(chalk.bgMagenta.black('Happy Birthday!') + (' ') + (chalk.blue('To') + (' ') + (chalk.yellow('You') + (chalk.white('!')))));
